03d2365c8869b4af89754e2de2afb8c6a89b8b26
TheSilkMiner <sir.karati@outlook.com>
2016-09-25 15:25:39 +0200

    Use API instead of direct calls in GUI event handler
    Sorry for no GPG signature, but Git doesn't want to run GPG nor any
    other software. Reason: unknown
    
    Signed-off-by: TheSilkMiner <sir.karati@outlook.com>

--- src/main/java/openmods/igw/impl/client/GuiOpenEventHandler.java
+++ src/main/java/openmods/igw/impl/client/GuiOpenEventHandler.java
@@ -1,5 +1,7 @@
 package openmods.igw.impl.client;
 
+import com.google.common.base.Preconditions;
+
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraftforge.client.event.GuiOpenEvent;
 
@@ -7,7 +9,9 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-import openmods.igw.OpenModsIGW;
+import openmods.igw.api.OpenModsIGWApi;
+import openmods.igw.api.service.IClassProviderService;
+import openmods.igw.api.service.IGuiService;
 
 @SideOnly(Side.CLIENT)
 public class GuiOpenEventHandler {
@@ -16,17 +20,23 @@ public class GuiOpenEventHandler {
 	@SideOnly(Side.CLIENT)
 	@SuppressWarnings("unused")
 	//@Explain("Called by Forge, not by us")
-	// TODO Move to API
 	public void onMainMenuOpen(final GuiOpenEvent event) {
 		if (!(event.gui instanceof GuiMainMenu)) return;
 
-		if (WarningGui.shallShow()) {
-			event.gui = new WarningGui();
+		final IGuiService it = Preconditions.checkNotNull(OpenModsIGWApi.get().serviceManager()
+				.obtainAndCastService(IGuiService.class));
+
+		if (it.shouldShow(IGuiService.GUIs.WARNING)) {
+			event.gui = it.construct(IGuiService.GUIs.WARNING);
 			return;
 		}
 
-		if (MismatchingVersionsGui.shouldShow()) {
-			event.gui = new MismatchingVersionsGui(OpenModsIGW.proxy().getMismatchingMods());
+		if (it.shouldShow(IGuiService.GUIs.MISMATCHING_MODS)) {
+			event.gui = it.construct(IGuiService.GUIs.MISMATCHING_MODS, Preconditions.checkNotNull(
+					Preconditions.checkNotNull(
+							OpenModsIGWApi.get().serviceManager().obtainAndCastService(IClassProviderService.class)
+					).proxy()
+			).getMismatchingMods());
 		}
 	}
 }
